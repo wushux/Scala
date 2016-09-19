1a431df4db1fa083863c36d4323746102aa0d1f7
Shuxian <wu.shux@husky.neu.edu>
2016-09-19 14:50:09 -0400

    classes added

---
+++ CSYE7200_wk2/src/main/scala/Ingest.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by Shuxian on 9/19/16.
+  */
+import scala.io.Source
+import scala.util.Try
+
+class Ingest[T: Ingestible] extends (Source => Iterator[Try[T]]) {
+  def apply(source: Source): Iterator[Try[T]] = source.getLines.toSeq.drop(1).map(e => implicitly[Ingestible[T]].fromString(e)).iterator
+}
+
+trait Ingestible[X] {
+  def fromString(w: String): Try[X]
+}
