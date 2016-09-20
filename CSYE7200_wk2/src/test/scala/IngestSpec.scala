8f1f4840f045b9d304b7a9100969b929783fd96a
Shuxian <wu.shux@husky.neu.edu>
2016-09-20 18:19:28 -0400

    update

--- CSYE7200_wk2/src/test/scala/IngestSpec.scala
+++ CSYE7200_wk2/src/test/scala/IngestSpec.scala
@@ -6,9 +6,6 @@ import org.scalatest.{FlatSpec, Matchers}
 import scala.io.Source
 import scala.util._
 
-/**
-  * Created by scalaprof on 9/13/16.
-  */
 class IngestSpec extends FlatSpec with Matchers {
 
   behavior of "ingest"
@@ -22,16 +19,16 @@ class IngestSpec extends FlatSpec with Matchers {
     val ingester = new Ingest[Int]()
     val xys: Iterator[Try[Int]] = ingester(source)
     for(xy<-xys){
-      xy should matchPattern{
-        case Success(42)=>
-      }
+        xy should matchPattern{
+          case Success(42)=>
+        }
     }
     // TODO check that xys has one element, consisting of Success(42) -- 10 points
   }
 
   it should "work for movie database" in {
     // NOTE that you expect to see a number of exceptions thrown. That's OK. We expect that some lines will not parse correctly.
-    Try(Source.fromFile("movie_metadata.csv")) match {
+    Try(Source.fromFile("src/test/movie_metadata.csv")) match {
       case Success(source) =>
         val ingester = new Ingest[Movie]()
         val mys: Seq[Try[Movie]] = (for (my <- ingester(source)) yield my.transform(
@@ -39,6 +36,7 @@ class IngestSpec extends FlatSpec with Matchers {
         )).toSeq
         val mos: Seq[Option[Movie]] = for (my <- mys) yield for (m <- my.toOption; if m.production.country == "New Zealand") yield m
         val ms = mos.flatten
+//        println("111111111",mos)
         ms.size shouldBe 4
         ms foreach { println(_) }
         source.close()
